7f79962cbfba8b352f0f22835edf4d0f18218c79
Andrey <a1081515@yandex.ru>
2020-10-14 19:34:45 +0300

    refactoring

--- components/CommentsSection/Comment/index.tsx
+++ components/CommentsSection/Comment/index.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link'
 import styles from './index.module.scss'
 
 interface Props {
-  comment1?: boolean
-  comment2?: boolean
-  comment3?: boolean
+  name: string
+  avatar: string
+  service: string,
+  message: string,
+  comments: number,
+  negative: number,
 }
 
 export default function Comment(props: Props) {
@@ -12,33 +15,22 @@ export default function Comment(props: Props) {
     <div className={styles.root}>
       <div className={styles.left}>
         <div className={styles.border}>
-          {props.comment1 ? <img src="img/CommentsSection/Comment/photo1.png"/> : null}
-          {props.comment2 ? <img src="img/CommentsSection/Comment/photo2.png"/> : null}
-          {props.comment3 ? <img src="img/CommentsSection/Comment/photo3.png"/> : null}
+           <img src={props.avatar}/>
         </div>
         <div className={styles.comment}>
           <div className={styles.inner}>
             <img src="img/icons/comment.svg" alt=''/>
-            {props.comment1 ? <div className={styles.positive}>64</div>: null}
-            {props.comment2 ? <div className={styles.positive}>126</div>: null}
-            {props.comment3 ? <div className={styles.positive}>15</div>: null}
-            {props.comment1 ? <div className={styles.negative}>2</div>: null}
-            {props.comment2 ? <div className={styles.negative}>1</div>: null}
+            <div className={styles.positive}>{props.comments}</div>
+            <div className={styles.negative}>{props.negative}</div>
           </div>
         </div>
       </div>
       <div>
-        {props.comment1 ? <div className={styles.name}>Татьяна Кравченко</div>: null}
-        {props.comment1 ? <Link href="/"><a><div className={styles.service}>Клининговые услуги</div></a></Link>: null}
-        {props.comment2 ? <div className={styles.name}>Михаил Терентьев</div>: null}
-        {props.comment2 ? <Link href="/"><a><div className={styles.service}>Фото и видео услуги</div></a></Link>: null}
-        {props.comment3 ? <div className={styles.name}>Елена Яковлева</div>: null}
-        {props.comment3 ? <Link href="/"><a><div className={styles.service}>Услуги перевода</div></a></Link>: null}
+     <div className={styles.name}>{props.name}</div>
+     <Link href="/"><a><div className={styles.service}>{props.service}</div></a></Link>
+
         <div className={styles.text}>
-          Данный текст не несёт ни какой смысловой нагрузки<br/> 
-          а является лишь имитацией наполнения текстовых блоков сайта. Данный<br/>
-          текст не несёт ни какой смысловой нагрузки а является лишь<br/> 
-          имитацией наполнения текстовых блоков сайта.
+          {props.message}
         </div>
       </div>
     </div>
--- components/CommentsSection/index.tsx
+++ components/CommentsSection/index.tsx
@@ -7,7 +7,10 @@ interface Props {}
 export default function CommentsSection(props: Props) {
   return (
     <div className={styles.root}>
-      <div className={styles.head}>Отзывы</div>
+      <div className={styles.commentBg}></div>
+      <div className={styles.commentContainer}>
+
+        <div className={styles.head}>Reviews</div>
       <div className={styles.floaters}>
         <img className={styles.icon} src="img/icons/posComments.svg" alt=''/>
         <div className={styles.text}>
@@ -21,9 +24,13 @@ export default function CommentsSection(props: Props) {
           </div>
         </div>
       </div>
-      <Comment comment1/>
-      <Comment comment2/>
-      <Comment comment3/>
+        <Comment name={'Ronald'} service={'Клининг'} avatar={'img/CommentsSection/Comment/photo1.png'} comments={64} negative={1} message={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lobortis consequat quis pulvinar suspendisse. Sed eu amet, auctor fermentum posuere convallis. Fusce lectus morbi purus suscipit velit. Adipiscing enim fames egestas nisl, tincidunt sit cursus posuere'}/>
+        <Comment name={'Татьяна Иванова'} service={'Клининг'} avatar={'img/CommentsSection/Comment/photo1.png'} comments={64} negative={1} message={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lobortis consequat quis pulvinar suspendisse. Sed eu amet, auctor fermentum posuere convallis. Fusce lectus morbi purus suscipit velit. Adipiscing enim fames egestas nisl, tincidunt sit cursus posuere'}/>
+        <Comment name={'Татьяна Петров'} service={'Клининг'} avatar={'img/CommentsSection/Comment/photo1.png'} comments={64} negative={1} message={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lobortis consequat quis pulvinar suspendisse. Sed eu amet, auctor fermentum posuere convallis. Fusce lectus morbi purus suscipit velit. Adipiscing enim fames egestas nisl, tincidunt sit cursus posuere'}/>
+      </div>
+      <div className={styles.orderingContainer}>
+
+      </div>
     </div>
   )
 }
