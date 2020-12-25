8bd8782279e999c1d6b216e90e3cd898a8ac981b
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2020-12-25 17:57:35 +0300

    StarRating without hover

--- components/ui/Inputs/Rating/index.tsx
+++ components/ui/Inputs/Rating/index.tsx
@@ -5,33 +5,20 @@ import styles from './index.module.scss'
 
 
 interface Props {
-
 }
 
 export default function Rating(props: Props) {
 
-  const [rating, setRating] = useState(1);
-  const [hover, setHover] = useState(null)
+  const [value, setValue] = useState(1)
 
   return (
-    <>
-      {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
-        return(
-          <label>
-            <input
-              type="radio"
-              name="rating"
-              value={ratingValue}
-              onClick={() => setRating(ratingValue)}
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(null)}
-            />
-            {i === 0 && <StarRating/>}
-            {ratingValue <= (hover || rating) ? <StarRating/> : <StarRatingStroke/>}
-          </label>
-        )
-      })}
-      </>
+  <label className={styles.stars}>
+      <input className={styles.input} value={value}/>
+      <div onClick={() => setValue(1)}><StarRating/></div>
+      <div onClick={() => value === 2 ? setValue(1) : setValue(2)}>{value >= 2 ? <StarRating/> : <StarRatingStroke/>}</div>
+      <div onClick={() => value === 3 ? setValue(2) : setValue(3)}>{value >= 3 ? <StarRating/> : <StarRatingStroke/>}</div>
+      <div onClick={() => value === 4 ? setValue(3) : setValue(4)}>{value >= 4 ? <StarRating/> : <StarRatingStroke/>}</div>
+      <div onClick={() => value === 5 ? setValue(4) : setValue(5)}>{value >= 5 ? <StarRating/> : <StarRatingStroke/>}</div>
+  </label>
   )
 }
