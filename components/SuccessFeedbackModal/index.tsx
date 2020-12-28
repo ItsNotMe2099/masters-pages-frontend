c3699a0dffd0df5ca8a40de99306ae6d7a57bef7
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2020-12-28 11:37:45 +0300

    MASTERS-69

---
+++ components/SuccessFeedbackModal/index.tsx
@@ -0,0 +1,23 @@
+import Button from "components/ui/Button";
+import Modal from "components/ui/Modal";
+import styles from './index.module.scss'
+
+interface Props {
+  isOpen: boolean
+  onRequestClose?: () => void
+}
+
+export default function SuccessTaskModal(props: Props) {
+
+  return (
+    <Modal{...props} className={styles.root} closeClassName={styles.close} >
+      <div className={styles.innards}>
+          <div><img src="/img/SuccessFeedbackModal/icon.svg" alt=""/></div>
+          <div className={styles.text}>Thank you for your feedback!</div>
+          <div className={styles.btnContainer}>
+            <Button type="button" red bold smallFont size="10px 45px">GO TO HOME PAGE</Button>
+          </div>
+      </div>
+    </Modal>
+  )
+}
