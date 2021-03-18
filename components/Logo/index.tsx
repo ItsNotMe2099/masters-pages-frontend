3fa3e0339a6affa0ba7f30a7516801cfcf19d4a4
Andrey <a1081515@yandex.ru>
2021-03-18 18:41:31 +0300

    Masters-102 wrap logo into link

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -4,19 +4,23 @@ import Link from 'next/link'
 import styles from './index.module.scss'
 
 interface Props {
-  color?: string
+  color?: string,
+  link?: string
 }
 
 export default function Logo(props: Props) {
   return (
-    <Link href="/">
-    <div className={styles.root}>
+    <Link href={props.link}>
+    <a className={styles.root}>
 
             <LogoSvg className={styles.iconDesktop} color={props.color}/>
             <LogoMobileSvg className={styles.iconMobile} color={props.color}/>
             <div className={`${styles.logoText}`} style={{...(props.color ? {color: props.color} : {})}}>Masters <span style={{...(props.color ? {color: props.color} : {})}}>Pages</span></div>
 
-        </div>
+        </a>
     </Link>
   )
+}
+Logo.defaultProps = {
+  link: '/'
 }
