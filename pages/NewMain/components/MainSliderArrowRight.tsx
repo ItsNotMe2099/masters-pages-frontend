656304a1e608a885003382d503f9f70445b0d1fa
manykhakv@it2g.ru <manykhakv@it2g.ru>
2021-03-31 13:47:28 +0400

    MASTERS-124 add viewport, fix camelcase error

--- pages/NewMain/components/MainSliderArrowRight.tsx
+++ pages/NewMain/components/MainSliderArrowRight.tsx
@@ -8,7 +8,7 @@ interface Props {
 function MainSliderArrowRight(props: Props) {
   return (
     <svg width="16" height="30" viewBox="0 0 16 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1.5 28L14.5 15L1.5 2" stroke="#F2F2F2" stroke-width="3" stroke-linejoin="round"/>
+      <path d="M1.5 28L14.5 15L1.5 2" stroke="#F2F2F2" strokeWidth="3" strokeLinejoin="round"/>
     </svg>
   )
 }
