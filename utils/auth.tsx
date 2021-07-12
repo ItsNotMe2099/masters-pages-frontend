a61922f05fc91de37f5050367c8d09102b6f532c
Andrey <a1081515@yandex.ru>
2021-07-12 19:19:34 +0300

    fix

--- utils/auth.tsx
+++ utils/auth.tsx
@@ -36,11 +36,11 @@ const getProfile = async (token, role) => {
 export const getAuthServerSide = ({redirect}: {redirect?: boolean} = {}) => wrapper.getServerSideProps(async (ctx) => {
   const token = auth(ctx);
 
-  if (ctx.req && ['masterspages.ca', 'masterspages.com', 'masterspages.ru'].includes(ctx.req.headers.host) && (!ctx.req.url || ctx.req.url === '/')) {
-    ctx.res.writeHead(302, {Location: "/"});
-    ctx.res.end();
-    return;
-  }
+  //if (ctx.req && ['masterspages.ca', 'masterspages.com', 'masterspages.ru'].includes(ctx.req.headers.host) && (!ctx.req.url || ctx.req.url === '/')) {
+ //   ctx.res.writeHead(302, {Location: "/"});
+  //  ctx.res.end();
+ //   return;
+ // }
   console.log("token", token)
   const mode = nextCookie(ctx)?.mode || 'client';
   const user = token ? await getUser(token) : null
