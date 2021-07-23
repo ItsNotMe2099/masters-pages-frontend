38ca221b6a9223bc05a97e6bc050bebc5feca849
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2021-07-23 18:44:10 +0300

    MASTERS-210

--- components/PublicProfile/components/view/CardPortfolio/components/PortfolioForm/index.tsx
+++ components/PublicProfile/components/view/CardPortfolio/components/PortfolioForm/index.tsx
@@ -30,7 +30,7 @@ let PortfolioForm = (props) => {
       />
       {profileTabs.length > 0 && <Field
         name="profileTabId"
-        label={t('createTask.filter.fieldCategory')}
+        label={t('createTask.fieldCategory')}
         component={SelectInput}
         size={'small'}
         options={profileTabs.map(item => ({label: item.title, value: item.id}))}
