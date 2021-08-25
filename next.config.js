43621ae4db0fed0414c6102571a8aa7c684daa51
Andrey <a1081515@yandex.ru>
2021-08-25 16:48:41 +0300

    fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const path = require('path');
-module.exports = {
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
@@ -43,4 +45,22 @@ module.exports = {
       ],
     }
   }
-}
+};
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+
+
