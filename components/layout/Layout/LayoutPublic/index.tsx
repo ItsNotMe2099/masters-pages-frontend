067cf3f4743245f6f80390c33d9cde076db25887
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2021-10-07 10:02:39 +0300

    MASTERS-350

--- components/layout/Layout/LayoutPublic/index.tsx
+++ components/layout/Layout/LayoutPublic/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.module.scss'
 import {useSelector, useDispatch} from 'react-redux'
 import MenuItem from 'components/layout/Layout/components/MenuItem'
 import {useTranslation} from 'i18n'
-import {default as React, ReactElement} from 'react'
+import {default as React, ReactElement, useState} from 'react'
 import {IRootState} from 'types'
 import Logo from 'components/Logo'
 
@@ -13,7 +13,9 @@ import ModeSelect from 'components/layout/Layout/components/ModeSelect'
 import MainSectionButton from 'pages/NewMain/components/Button'
 import {signInOpen, signUpOpen} from 'components/Modal/actions'
 import Button from 'components/PublicProfile/components/Button'
-import { LangSelect } from '../LayoutAuthorized/mobile/Header/components/LangSelect'
+import LangSelect from 'pages/NewMain/components/Header/components/LangSelect'
+import MenuMobile from 'components/svg/MenuMobile'
+import MenuMobileClose from 'components/svg/MenuMobileClose'
 
 
 interface Props {
@@ -30,6 +32,22 @@ export default function LayoutPublic(props: Props) {
   const {t} = useTranslation('common');
   const dispatch = useDispatch()
 
+  const [isMenuMobileOpen, setMenuMobileOpen] = useState(false)
+  const handleOpenMobileMenu = () => {
+    if (process.browser) {
+      document.body.classList.add('modal-open')
+    }
+
+    setMenuMobileOpen(true)
+  }
+
+  const handleCloseMobileMenu = () => {
+    if (process.browser) {
+      document.body.classList.remove('modal-open')
+    }
+    setMenuMobileOpen(false)
+  }
+
   return (
     <div className={`${styles.root}`}>
       <div  className={styles.header}>
@@ -40,6 +58,10 @@ export default function LayoutPublic(props: Props) {
           <div className={styles.title}>
             {title}
           </div>
+          <div className={styles.menuMobile}>
+          <LangSelect isAuth={false}/>
+          {!isMenuMobileOpen ? <MenuMobile color='#c4c4c4' onClick={handleOpenMobileMenu} className={styles.menuMobileBtn}/> : <MenuMobileClose color='#c4c4c4' onClick={handleCloseMobileMenu}/>}
+        </div>
           <div className={styles.actions}>
             <LangSelect isAuth={false}/>
             <div className={styles.actionsButtons}>
@@ -49,6 +71,18 @@ export default function LayoutPublic(props: Props) {
           </div>
 
       </div>
+      {isMenuMobileOpen && 
+      <div className={styles.dropdownMobile}>
+          <div className={styles.actionsMobile}>
+          <div className={styles.titleMobile}>
+            {title}
+          </div>
+          <div className={styles.actionsButtons}>
+            <Button className={styles.actionButton} size={'small'}  onClick={() => dispatch(signInOpen())}>{t('menu.signIn')}</Button>
+             <Button className={styles.actionButton} size={'small'} color={'red'} onClick={() => dispatch(signUpOpen())}>{t('menu.signUp')}</Button>
+            </div>
+        </div>
+      </div>}
       <div className={styles.container}>
         {children}
       </div>
