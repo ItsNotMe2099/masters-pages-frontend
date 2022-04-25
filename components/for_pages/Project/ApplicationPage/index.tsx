f9c5d2525dc7ccffffd5b59650d7bfb396213785
Nikita <wrongnmail@yandex.ru>
2022-04-25 18:26:15 +0300

    ApplicationPage component Buttons (not complete)

--- components/for_pages/Project/ApplicationPage/index.tsx
+++ components/for_pages/Project/ApplicationPage/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 import styles from './index.module.scss'
 import {IProject} from 'data/intefaces/IProject'
 import StarRatings from 'react-star-ratings'
@@ -9,23 +9,33 @@ import {ApplicationStatus, IApplication} from 'data/intefaces/IApplication'
 import classNames from 'classnames'
 import Avatar from 'components/ui/Avatar'
 import ProfileStatus from 'components/ui/ProfileStatus'
-import {getMaxAge} from 'next/dist/server/image-optimizer'
 import {getAge} from 'utils/date'
 import LanguageListItem from 'components/PublicProfile/components/view/CardLanguages/components/LanguageListItem'
 import {format} from 'date-fns'
+import VolunteerStats from '../ProjectModal/Tabs/TabApplication/VolunteerStats'
+import { useDispatch } from 'react-redux'
+import { confirmModalClose, confirmOpen, modalClose } from 'components/Modal/actions'
+import Button from 'components/ui/Button'
 
 interface Props {
   project: IProject
   application: IApplication
   index?: number
   total?: number
+  modal?: boolean
+  onStatusChange?: (newStatus: ApplicationStatus) => void
+  currentTab?: ApplicationStatus
+}
+interface ButtonsProps {
+  application?: IApplication
+  onViewClick?: () => void
 }
 const RequirementStatus = (props: {name: string, success: boolean}) => {
   return <div className={classNames(styles.requirementStatus, {[styles.success]: props.success, [styles.failed]: !props.success})}>
     {props.name} <img src={`/img/Project/requirements_${props.success ? 'success' : 'failed'}.svg`}/>
   </div>
 }
-const ApplicationPage = ({application, index, total, project, ...props}: Props) => {
+const ApplicationPage = ({application, index, total, project, modal, onStatusChange, currentTab, ...props}: Props) => {
   const {t} = useTranslation();
   const appContext = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
@@ -52,6 +62,104 @@ const ApplicationPage = ({application, index, total, project, ...props}: Props) 
     return false;
   }
   const profileLink = `/id${application.profile.id}`
+
+  const dispatch = useDispatch()
+
+  const handleConfirm = (status: ApplicationStatus) => {
+    onStatusChange(status)
+    dispatch(confirmModalClose())
+  }
+
+  const description = (newStatus: ApplicationStatus, button?: string) => {
+    switch(newStatus){
+      case ApplicationStatus.Completed:
+        return 'Volunteer involvement will be marked as “completed” and will be ended. Do you want to proceed?'
+      case ApplicationStatus.Invited:
+        return 'Invitation to join the project will be sent to the applicant. Do you want to proceed?'
+      case ApplicationStatus.Shortlist:
+        if(button === 'CANCEL INVITATION'){
+          return 'Invitation to join the project will be withdrawn. Do you want to proceed?'
+        }
+        else{
+          return 'The application will be “shortlisted”. Do you want to proceed?'
+        }
+      case ApplicationStatus.RejectedByCompany:
+        return 'Volunteer involvement will be cancelled. Do you want to proceed?'
+    }
+  }
+
+  const confirmData = (status: ApplicationStatus, button?: string) => {
+    return  {title: ' ', description: description(status, button), onConfirm: () => {handleConfirm(status)}, onCancel: () => {dispatch(confirmModalClose())}}
+  }
+
+  const Buttons = (props: ButtonsProps) => {
+
+    switch(currentTab){
+      case ApplicationStatus.Applied:
+        return (
+          <div className={styles.btns}>
+            <Button onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.Shortlist)))} type='button' projectBtn='default'>
+              SHORTLIST
+            </Button>
+            <Button 
+            onClick={() => onStatusChange(ApplicationStatus.RejectedByCompany)} 
+            type='button' projectBtn='red'>REJECT</Button>
+          </div>
+        )
+      case ApplicationStatus.Shortlist:
+        return (
+          <div className={styles.btns}>
+            <Button onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.Invited)))}  type='button' projectBtn='default'>
+              INVITE
+            </Button>
+            <Button 
+            onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.RejectedByCompany)))}
+            type='button' projectBtn='red'>REJECT</Button>
+          </div>
+        )
+      case ApplicationStatus.Invited:
+        return (
+          <div className={styles.btns}>
+            <Button 
+            onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.Shortlist, 'CANCEL INVITATION')))}
+            type='button' projectBtn='red'>CANCEL INVITATION</Button>
+          </div>
+        )
+      case ApplicationStatus.Execution:
+        return (
+          <div className={styles.btns}>
+            <Button onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.Completed)))}
+            type='button' projectBtn='default'>
+              COMPLETE
+            </Button>
+            <Button 
+            onClick={() => dispatch(confirmOpen(confirmData(ApplicationStatus.RejectedByCompany)))}
+            type='button' projectBtn='red'>REJECT</Button>
+          </div>
+          )
+        case ApplicationStatus.Completed:
+          return (
+            <div className={styles.btnsCompleted}>
+              <Button type='button' projectBtn='default'>
+                REVIEW
+              </Button>
+              <Button type='button' projectBtn='default'>RECOMMEND</Button>
+              <Button projectBtn='recycleBin'><img src='/img/icons/recycle-bin.svg' alt=''/></Button>
+            </div>
+          )
+        case ApplicationStatus.RejectedByCompany:
+          return (
+            <div className={styles.btns}>
+              <Button 
+              type='button' projectBtn='default'>
+                RESTORE
+              </Button>
+              <Button projectBtn='recycleBin'><img src='/img/icons/recycle-bin.svg' alt=''/></Button>
+            </div>
+          )
+    }
+  }
+
   return (
    <div className={styles.root}>
      <div className={styles.leftSide}>
@@ -99,6 +207,9 @@ const ApplicationPage = ({application, index, total, project, ...props}: Props) 
             <div className={styles.statusBlock}>
             <div className={styles.status}><img src={'/img/Project/folder.svg'}/> {application.status}</div>
             </div>
+            {modal && 
+              <Buttons application={application}/>
+            }
             <div className={styles.actions}></div>
           </div>
         </div>
@@ -113,6 +224,9 @@ const ApplicationPage = ({application, index, total, project, ...props}: Props) 
      </div>
      </div>
      <div className={styles.rightSide}>
+       {modal &&
+        <VolunteerStats/>
+        }
        <div className={styles.requirements}>
          <div className={styles.header}>Requirements Check</div>
          <div className={styles.columns}>
--- components/for_pages/Project/ProjectModal/Tabs/TabApplication/TabApplicationCard/index.tsx
+++ components/for_pages/Project/ProjectModal/Tabs/TabApplication/TabApplicationCard/index.tsx
@@ -10,6 +10,7 @@ import LanguageListItem from 'components/PublicProfile/components/view/CardLangu
 import { confirmModalClose, confirmOpen, modalClose } from 'components/Modal/actions'
 import { useDispatch } from 'react-redux'
 import VolunteerStats from '../VolunteerStats'
+import StarRatings from 'react-star-ratings'
 
 
 interface Props {
@@ -137,6 +138,31 @@ const TabApplicationCard = ({application, currentTab, onStatusChange, ...props}:
             <Avatar image={props.profile.photo} size='largeSquare'/>
             <Button type='button' className={styles.details}><img src='/img/projects/account-details.svg' alt=''/></Button>
           </div>
+          <div className={styles.icons}>
+              <div className={styles.stat}>
+              <img src="/img/SearchTaskPage/icons/case.svg" alt=''/>
+              <div>{application.profile.tasksCount || 0}</div>
+              </div>
+              <div className={styles.stat}>
+              <img src="/img/SearchTaskPage/icons/like.svg" alt=''/>
+              <div>{application.profile.feedbacksCount || 0}</div>
+              </div>
+            </div>
+            <div className={styles.stars}>
+              <StarRatings
+                rating={application.profile.rating || 0}
+                starRatedColor="#F2B705"
+                starEmptyColor={'#616161'}
+                numberOfStars={5}
+                name='rating'
+                svgIconPath={'M4.08729 13.7644C3.74325 13.9408 3.35287 13.6316 3.42239 13.2367L4.16216 9.0209L1.02213 6.02971C0.728899 5.74985 0.88131 5.23824 1.27437 5.18298L5.63993 4.56264L7.58651 0.706016C7.7621 0.358411 8.23716 0.358411 8.41274 0.706016L10.3593 4.56264L14.7249 5.18298C15.1179 5.23824 15.2704 5.74985 14.9771 6.02971L11.8371 9.0209L12.5769 13.2367C12.6464 13.6316 12.256 13.9408 11.912 13.7644L7.99829 11.7536L4.0864 13.7644H4.08729Z'}
+                svgIconViewBox={'0 0 16 14'}
+                starDimension={'16px'}
+                starSpacing={'1px'}
+
+              />
+              <div className={styles.comments}>({application.profile.rating || 0})</div>
+            </div>
         </div>
         <div className={styles.middle}>
           <div className={styles.top}>
