0045c7c057c8ebb7e23c09c3febbcfe60b1efd2b
Nikita <wrongnmail@yandex.ru>
2022-06-09 18:10:49 +0300

    search

--- components/for_pages/GuestPage/GuestFilter/index.tsx
+++ components/for_pages/GuestPage/GuestFilter/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.scss'
 import { useTranslation } from 'next-i18next'
 import InputSearch from 'components/ui/Inputs/InputSearch'
-import { useState } from 'react'
+import { ReactElement, useState } from 'react'
 import classNames from 'classnames'
 import Filter from '../FilterForm'
 
@@ -10,6 +10,7 @@ interface Props {
   state?: boolean
   onClick?: () => void
   filter?: 'projects' | 'companies'
+  search?: () => ReactElement
 }
 const GuestFilter = (props: Props) => {
 
@@ -24,7 +25,7 @@ const GuestFilter = (props: Props) => {
   return (
     <div className={styles.root}>
       <div className={styles.top}>
-        <InputSearch/>
+        {props.search()}
         <div className={classNames(styles.filtersBtn, {[styles.visible]: isVisible})} onClick={handleClick}>
           <div className={styles.icon}><img src='/img/icons/filter.svg' alt=''/></div>
           <div className={styles.text}>Filters</div>
--- components/ui/Inputs/InputSearch/index.tsx
+++ components/ui/Inputs/InputSearch/index.tsx
@@ -4,34 +4,29 @@ import styles from './index.module.scss'
 
 interface Props {
   placeholder?: string
-  onChange?: (value) => void
+  onChange?: (e) => void
   onClick?: () => void
   searchValue?: string
 }
 
 export default function InputSearch(props: Props) {
-  const [value, setValue] = useState('')
-  useEffect(() => {
-    if (props.searchValue && !value) {
-      setValue(props.searchValue)
-    }
-  }, [props.searchValue])
+  const [value, setValue] = useState(props.searchValue)
 
   const handleSearch = (e) => {
     setValue(e.currentTarget.value)
     props.onChange(e.currentTarget.value)
   }
   return (
-    <form className={styles.root} action="/search">
+    <form className={styles.root}>
       <input
         name="query"
         type="text"
-        value={value}
+        value={props.searchValue}
         autoComplete={'off'}
-        onChange={handleSearch}
+        onChange={props.onChange}
         placeholder={props.placeholder}
       />
-        <Button className={styles.btn}>
+        <Button className={styles.btn} onClick={props.onClick}>
           <img src="/img/icons/search.svg" alt="" />
         </Button>
     </form>
