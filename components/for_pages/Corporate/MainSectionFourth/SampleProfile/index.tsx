9380a112fd14660cec55398c64bf919eac1a3837
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2022-10-25 14:09:38 +0600

    fourth section edit

--- components/for_pages/Corporate/MainSectionFourth/SampleProfile/index.tsx
+++ components/for_pages/Corporate/MainSectionFourth/SampleProfile/index.tsx
@@ -1,33 +1,27 @@
-
 import styles from 'components/for_pages/Corporate/MainSectionFourth/SampleProfile/index.module.scss'
 import { useTranslation } from 'next-i18next'
 import React from 'react'
+import { getMediaPath } from 'utils/media'
 import Link from 'next/link'
 
-interface IItem {
-  image: string
-  link: string
-}
-
 interface Props{
-  item: IItem
+  item: any
+  type?: string
 }
 
 
-const SampleProfile = ({item}: Props) => {
+const SampleProfile = ({item, type}: Props) => {
 
   const { t, i18n } = useTranslation('common')
 
   return (
     <div className={styles.root}>
       <div className={styles.image}>
-        <img src='/img/MainVolunteer/sample1.png' alt=''/>
+        <img className={styles.small} src={getMediaPath(item.photoObject?.urlS3)} alt=''/>
       </div>
-      <div>
-        <div className={styles.spot}>
-          {t('newMainVolunteer.spotOn')}
-        </div>
-        <Link href={item.link}>
+      <div className={styles.view}>
+        <div className={styles.type}>{type}</div>
+        <Link href={`${item.profileLink || `/id${item.id}`}`}>
         <a className={styles.viewProfile}>
           <img src='/img/icons/viewProfile.svg' alt=''/>
           <div>{t('profileComponent.viewProfile')}</div>
--- components/for_pages/Corporate/MainSectionFourth/index.tsx
+++ components/for_pages/Corporate/MainSectionFourth/index.tsx
@@ -1,23 +1,13 @@
 import styles from 'components/for_pages/Corporate/MainSectionFourth/index.module.scss'
 import { useTranslation } from 'next-i18next'
 import React, { useEffect, useState } from 'react'
-import Video from 'components/for_pages/Corporate/MainSectionFourth/Video'
-import SampleProfile from 'components/for_pages/MainUserPage/MainSectionFourth/SampleProfile'
+import SampleProfile from 'components/for_pages/Corporate/MainSectionFourth/SampleProfile'
 import MainSectionButton from 'components/for_pages/Corporate/Button'
 import { useDispatch } from 'react-redux'
 import { signUpOpen } from 'components/Modal/actions'
 import { IProfile } from 'data/intefaces/IProfile'
 import ProfileRepository from 'data/repositories/ProfileRepostory'
-
-interface IReview {
-  image: string
-  name: string
-  text: string
-}
-
-interface ReviewProps {
-  item: IReview
-}
+import { CONTACTS } from 'types'
 
 
 const MainSectionFourth = (props) => {
@@ -26,11 +16,9 @@ const MainSectionFourth = (props) => {
 
   const dispatch = useDispatch()
 
-  const videos = [
-    {url: 'https://youtu.be/O3i3XX6Fj-A', title: 'About Masterspages services for organizations'},
-    {url: 'https://youtu.be/FMa-WUJKw0k', title: 'How to set up an organisation\' profile'},
-  {url: 'https://youtu.be/bF1GaeD4JZo', title: 'How to launch a volunteer project ad'},
-   {url: 'https://youtu.be/jt7ehRqFj3I', title: 'How to manage a volunteer project'},]
+  const files = [
+    {url: '/files/MastersPages corporate service.pdf', title: 'MastersPages corporate service'},
+    {url: '/files/Masterspages onepager.pdf', title: 'Masterspages onepager'},]
 
   const url = '/api/profile/for-main-page'
   //const { data: data } = useSWR(url)
@@ -45,57 +33,43 @@ const MainSectionFourth = (props) => {
     })
   }, [])
 
-  const reviews = [
-    {image: '/img/MainVolunteer/reviews-avatar.png', name: 'Mark Smith', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis sed orci in nec luctus fermentum et. Ac pulvinar in ac eros, lectus viverra urna. Mauris suscipit proin libero,'},
-  ]
-
-  const Review = ({item}: ReviewProps) => {
-    return (
-      <div className={styles.review}>
-        <div className={styles.top}>
-          <div className={styles.avatar}>
-            <img src={item.image} alt=''/>
-          </div>
-          <div className={styles.name}>
-            {item.name}
-          </div>
-        </div>
-        <div className={styles.text}>
-          {item.text}
-        </div>
-      </div>
-    )
-  }
 
   return (
     <div className={styles.root}>
       <div className={styles.container}>
       <div className={styles.videos}>
-        <div className={styles.title}>{t('mainPage.fourthSection.discoverMore')}</div>
-        {videos.map(video =>
-          <div className={styles.video}>
-            <Video url={video.url} title={video.title}/>
-          </div>
+        <div className={styles.title}>Files</div>
+        {files.map(item =>
+          <a className={styles.item} 
+          href={item.url} 
+          target='_blank' 
+          download={item.url}>
+            <div className={styles.image}>
+              <img src={'/img/files/pdf.png'} alt=''/>
+            </div>
+            <span>{item.title}</span>
+          </a>
         )}
       </div>
       <div className={styles.separator}></div>
       <div className={styles.profiles}>
-        <div className={styles.title}>{t('newMainVolunteer.sampleProfiles')}</div>
-        {data && data.slice(0, 3).map(item =>
-          <SampleProfile item={item}/>
+        <div className={styles.title}>Profile examples</div>
+        {data && data.slice(0, 3).map((item, index) =>
+          <SampleProfile item={item} type={index === 1 ? 'Corporate profile' : index === 2 ? 'Volunteer project' : 'Volunteer profile'}/>
         )}
       </div>
       <div className={styles.separator}></div>
       <div className={styles.reviews}>
-        <div className={styles.title}>{t('newMainVolunteer.customerReviews')}</div>
-        {reviews.map((item, index) =>
-          <Review item={item} key={index}/>
-        )}
+        <div className={styles.title}>Contact us</div>
+        <div className={styles.mail}>
+          <img src='/img/Corporate/icons/mail.svg' alt=''/>
+          <a className={styles.email} href={`mailto:${CONTACTS.email}`}>{CONTACTS.email} </a>
+        </div>
       </div>
       </div>
       <div className={styles.btns}>
-            <MainSectionButton size="normal" color="outlineRed" href='/guestpage'>{t('newMainVolunteer.guestAccess')}</MainSectionButton>
-            <MainSectionButton size="normal" color="red" onClick={() => dispatch(signUpOpen())}>{t('newMainVolunteer.freeSignUp')}</MainSectionButton>
+            <MainSectionButton size="normal" color="outlineYellow" href='/guestpage'>{t('newMainVolunteer.guestAccess')}</MainSectionButton>
+            <MainSectionButton size="normal" color="yellow" onClick={() => dispatch(signUpOpen())}>{t('newMainVolunteer.freeSignUp')}</MainSectionButton>
       </div>
     </div>
   )
