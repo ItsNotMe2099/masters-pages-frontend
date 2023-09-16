3228503ea3610487394f606882a120afdd49bb74
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-09-16 17:14:58 +0300

    fix ProjectList

--- components/for_pages/Project/ProjectList/index.tsx
+++ components/for_pages/Project/ProjectList/index.tsx
@@ -1,28 +1,28 @@
-import {confirmChangeData, confirmModalClose, confirmOpen, modalClose, projectOpen} from 'components/Modal/actions'
+import { confirmChangeData, confirmModalClose, confirmOpen, modalClose, projectOpen } from 'components/Modal/actions'
 import Loader from 'components/ui/Loader'
 import Tabs from 'components/ui/Tabs'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import * as React from 'react'
-import {useEffect, useMemo, useState} from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import {IRootState} from 'types'
+import { IRootState } from 'types'
 import styles from './index.module.scss'
-import {useDispatch, useSelector} from 'react-redux'
-import {TabSelect} from 'components/TabSelect'
-import {useTranslation} from 'next-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { TabSelect } from 'components/TabSelect'
+import { useTranslation } from 'next-i18next'
 import Layout from 'components/layout/Layout'
-import {getAuthServerSide} from 'utils/auth'
+import { getAuthServerSide } from 'utils/auth'
 import Modals from 'components/layout/Modals'
 import Button from 'components/ui/Button'
-import {IProfile, ProfileRole} from 'data/intefaces/IProfile'
+import { IProfile, ProfileRole } from 'data/intefaces/IProfile'
 import ProjectModal from 'components/for_pages/Project/ProjectModal'
-import {useAppContext} from 'context/state'
-import {IProject, ProjectStatus} from 'data/intefaces/IProject'
+import { useAppContext } from 'context/state'
+import { IProject, ProjectStatus } from 'data/intefaces/IProject'
 import ProjectCard from 'components/for_pages/Project/ProjectCard'
 import ProjectRepository from 'data/repositories/ProjectRepository'
-import {IProjectCounts} from 'data/intefaces/IProjectCounts'
+import { IProjectCounts } from 'data/intefaces/IProjectCounts'
 import OrganizationRepository from 'data/repositories/OrganizationRepository'
-import {IOrganization} from 'data/intefaces/IOrganization'
+import { IOrganization } from 'data/intefaces/IOrganization'
 import ProjectActions from "components/for_pages/Project/ProjectActions";
 
 interface Props {
@@ -30,10 +30,10 @@ interface Props {
 }
 
 const ProjectList = (props: Props) => {
-  const {t} = useTranslation('common')
+  const { t } = useTranslation('common')
   const router = useRouter()
   const dispatch = useDispatch()
-  const {projectType} = router.query
+  const { projectType } = router.query
   const appContext = useAppContext()
   const currentProfile = appContext.profile
   const loading = false
@@ -47,16 +47,16 @@ const ProjectList = (props: Props) => {
   const modalKey = useSelector((state: IRootState) => state.modal.modalKey)
   const [currentProjectEditId, setCurrentProjectEditId] = useState<number>(null)
   useEffect(() => {
-  } ,[])
+  }, [])
   const limit = 30;
   const tabs = useMemo(
     () => ([
-      {name: t('personalArea.tabProjects.menu.draft'), key: ProjectStatus.Draft},
-      {name: t('personalArea.tabProjects.menu.intake'), key: ProjectStatus.Published, badge:  currentProfile.notificationsForIntakeProjectsCount},
-      {name: t('personalArea.tabProjects.menu.paused'), key: ProjectStatus.Paused, badge:   currentProfile.notificationsForPausedProjectsCount},
-      {name: t('personalArea.tabProjects.menu.execution'), key: ProjectStatus.Execution, badge:  currentProfile.notificationsForExecutionProjectsCount},
-      {name: t('personalArea.tabProjects.menu.completed'), key: ProjectStatus.Completed, badge:  currentProfile.notificationsForCompletedProjectsCount},
-      {name: t('personalArea.tabProjects.menu.cancelled'), key: ProjectStatus.Canceled, badge:  currentProfile.notificationsForCanceledProjectsCount},
+      { name: t('personalArea.tabProjects.menu.draft'), key: ProjectStatus.Draft },
+      { name: t('personalArea.tabProjects.menu.intake'), key: ProjectStatus.Published, badge: currentProfile.notificationsForIntakeProjectsCount },
+      { name: t('personalArea.tabProjects.menu.paused'), key: ProjectStatus.Paused, badge: currentProfile.notificationsForPausedProjectsCount },
+      { name: t('personalArea.tabProjects.menu.execution'), key: ProjectStatus.Execution, badge: currentProfile.notificationsForExecutionProjectsCount },
+      { name: t('personalArea.tabProjects.menu.completed'), key: ProjectStatus.Completed, badge: currentProfile.notificationsForCompletedProjectsCount },
+      { name: t('personalArea.tabProjects.menu.cancelled'), key: ProjectStatus.Canceled, badge: currentProfile.notificationsForCanceledProjectsCount },
     ]).map(item => {
       return {
         ...item,
@@ -64,13 +64,13 @@ const ProjectList = (props: Props) => {
       }
     }),
     [currentProfile.role, counts,
-      currentProfile.notificationNewApplicationCount,
-      currentProfile.notificationApplicationShortlistCount,
-      currentProfile.notificationApplicationCompleteRequestCount,
-      currentProfile.notificationApplicationCompletedCount,
-      currentProfile.notificationApplicationExecutionCount,
-      currentProfile.notificationApplicationRejectedByVolunteerCount,
-      currentProfile.notificationApplicationRejectedByCompanyCount,
+    currentProfile.notificationNewApplicationCount,
+    currentProfile.notificationApplicationShortlistCount,
+    currentProfile.notificationApplicationCompleteRequestCount,
+    currentProfile.notificationApplicationCompletedCount,
+    currentProfile.notificationApplicationExecutionCount,
+    currentProfile.notificationApplicationRejectedByVolunteerCount,
+    currentProfile.notificationApplicationRejectedByCompanyCount,
     ]
   )
 
@@ -107,7 +107,7 @@ const ProjectList = (props: Props) => {
     setPage(page + 1);
     const data = await ProjectRepository.fetchByStatus(projectType as ProjectStatus, page + 1, limit);
     if (data) {
-      setProjects(data.data);
+      setProjects(projects => [...projects, ...data.data])
       setTotal(data.total)
     }
   }
@@ -117,7 +117,7 @@ const ProjectList = (props: Props) => {
   }
 
   const appStatus = (status: string) => {
-    return {status: status}
+    return { status: status }
   }
 
 
@@ -176,7 +176,7 @@ const ProjectList = (props: Props) => {
     dispatch(confirmOpen({
       description: `${t('task.confirmDelete')} «${project.title}»?`,
       onConfirm: async () => {
-        dispatch(confirmChangeData({loading: true}))
+        dispatch(confirmChangeData({ loading: true }))
         await ProjectRepository.delete(project.id)
         appContext.projectDeleteState$.next(project)
         dispatch(confirmModalClose())
@@ -184,50 +184,51 @@ const ProjectList = (props: Props) => {
     }))
   }
   return (
-      <div className={styles.root}>
-        {currentProfile.role === ProfileRole.Corporate &&
-          <div className={styles.actions}>
-            <Button red={true} bold={true} size={'12px 40px'}
-                    type={'button'} onClick={handleCreateProject}>{t('personalArea.tabProjects.menu.create')}</Button>
-          </div>}
-        <div className={styles.desktop}>
-          <Tabs style={'fullWidthRound'} tabs={tabs.map((tab => {
-            const statResult = counts[tab.key]
-            console.log("TabRender", tab);
-            return {
-              ...tab,
-              name: `${tab.name} (${statResult ? statResult : 0})`
-            }
-          }))} activeTab={projectType as string}/>
-        </div>
-        <div className={styles.mobile}>
-          <TabSelect style='projectStatus' tabs={tabs.map((tab => {
-            const statResult = counts[tab.key]
-            return {
-              ...tab,
-              name:  `${tab.name} (${statResult ? statResult : 0})`
-            }
-          }))} activeTab={projectType as string}/>
-
-        </div>
-        <div className={styles.projects}>
-          {(loading && total === 0) && <Loader/>}
-          {total > 0 && <InfiniteScroll
-            dataLength={projects.length} //This is important field to render the next data
-            next={handleScrollNext}
-            hasMore={total > projects.length}
-            loader={loading ? <Loader/> : null}>
-            {projects.sort((a, b) => +new Date(b.updatedAt) - +new Date(a.updatedAt)).map(project =>
-              <ProjectCard
-                key={project.id}
-                actions={<ProjectActions onDelete={() => handleDelete(project)} onModalOpen={(mode) => props.onModalOpen(project, mode)}
-                                         actionsType={actionsType} status={projectType} project={project}/>}
-                project={project}/>
-            )}
-          </InfiniteScroll>
+    <div className={styles.root}>
+      {currentProfile.role === ProfileRole.Corporate &&
+        <div className={styles.actions}>
+          <Button red={true} bold={true} size={'12px 40px'}
+            type={'button'} onClick={handleCreateProject}>{t('personalArea.tabProjects.menu.create')}</Button>
+        </div>}
+      <div className={styles.desktop}>
+        <Tabs style={'fullWidthRound'} tabs={tabs.map((tab => {
+          const statResult = counts[tab.key]
+          console.log("TabRender", tab);
+          return {
+            ...tab,
+            name: `${tab.name} (${statResult ? statResult : 0})`
           }
-        </div>
+        }))} activeTab={projectType as string} />
       </div>
+      <div className={styles.mobile}>
+        <TabSelect style='projectStatus' tabs={tabs.map((tab => {
+          const statResult = counts[tab.key]
+          return {
+            ...tab,
+            name: `${tab.name} (${statResult ? statResult : 0})`
+          }
+        }))} activeTab={projectType as string} />
+
+      </div>
+      <div className={styles.projects}>
+        {(loading && total === 0) && <Loader />}
+        {total > 0 && <InfiniteScroll
+          scrollableTarget="scrollableDiv"
+          dataLength={projects.length} //This is important field to render the next data
+          next={handleScrollNext}
+          hasMore={total > projects.length}
+          loader={loading ? <Loader /> : null}>
+          {projects.map(project =>
+            <ProjectCard
+              key={project.id}
+              actions={<ProjectActions onDelete={() => handleDelete(project)} onModalOpen={(mode) => props.onModalOpen(project, mode)}
+                actionsType={actionsType} status={projectType} project={project} />}
+              project={project} />
+          )}
+        </InfiniteScroll>
+        }
+      </div>
+    </div>
 
   )
 }
