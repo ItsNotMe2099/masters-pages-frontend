9bb516a5f6dddfcf84cc6daa2cb47324aa7bb5e7
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-12-08 17:10:54 +0300

    fix

--- pages/Posts/index.tsx
+++ pages/Posts/index.tsx
@@ -1,24 +1,24 @@
-import {getAuthServerSide} from 'utils/auth'
+import { getAuthServerSide } from 'utils/auth'
 import styles from './index.module.scss'
 import { useSelector, useDispatch } from 'react-redux'
-import {default as React, useState} from 'react'
+import { default as React, useState } from 'react'
 import { useTranslation } from 'next-i18next'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import Layout from 'components/layout/Layout'
 
-import { IRootState} from 'types'
+import { IRootState } from 'types'
 import PostModal from 'components/Post/PostModal'
-import { modalClose, postEditOpen} from 'components/Modal/actions'
+import { modalClose, postEditOpen } from 'components/Modal/actions'
 import Button from 'components/ui/Button'
 import Modals from 'components/layout/Modals'
 import PostList from 'components/Post/PostList'
 import { PostsTabType } from 'components/for_pages/Project/ProjectModal/Tabs/TabPosts'
 import Tabs from 'components/for_pages/Project/ProjectModal/Tabs/TabVolunteers/Tabs'
-interface Props{
+interface Props {
   profileId?: number
 }
 const PostsPage = (props: Props) => {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
   const router = useRouter()
   const dispatch = useDispatch()
   const modalKey = useSelector((state: IRootState) => state.modal.modalKey)
@@ -54,17 +54,17 @@ const PostsPage = (props: Props) => {
     <Layout>
       <div className={styles.container}>
         <div className={styles.header}>
-        <Tabs className={styles.tabs} onChange={(item) => handleChange(item)} style={'reports'} tabs={tabs} activeTab={currentTab} />
-          <Button red={true} bold={true} size={'12px 40px'} type={'button'} onClick={handleCreate}>{t('post.createPost')}</Button>
+          <Tabs className={styles.tabs} onChange={(item) => handleChange(item)} style={'reports'} tabs={tabs} activeTab={currentTab} />
+          <Button className={styles.btn} red={true} bold={true} size={'12px 40px'} type={'button'} onClick={handleCreate}>{t('post.createPost')}</Button>
         </div>
-       <PostList allPosts={currentTab === PostsTabType.All} onEdit={handleEdit} />
+        <PostList allPosts={currentTab === PostsTabType.All} onEdit={handleEdit} />
 
       </div>
-      {modalKey === 'postEditOpen' && <PostModal currentEditPost={currentEditPost} isOpen={true} onClose={() => dispatch(modalClose())}/>}
+      {modalKey === 'postEditOpen' && <PostModal currentEditPost={currentEditPost} isOpen={true} onClose={() => dispatch(modalClose())} />}
 
-      <Modals/>
+      <Modals />
     </Layout>
   )
 }
-export const getServerSideProps = getAuthServerSide({redirect: true})
+export const getServerSideProps = getAuthServerSide({ redirect: true })
 export default PostsPage
