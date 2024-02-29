9b048175319288cb356bc0c56d23ce8c5b7d8dfa
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2024-02-29 18:01:54 +0300

    calendar LphoneWidth

--- components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/ProjectCalendarSideBarCalendar/index.tsx
+++ components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/ProjectCalendarSideBarCalendar/index.tsx
@@ -4,7 +4,7 @@ import CalendarSideBarCalendarCell
   from 'components/Calendar/components/CalendarSideBar/CalendarSideBarCalendar/CalendarSideBarCalendarCell'
 import CalendarArrowLeft from 'components/svg/CalendarArrowLeft'
 import CalendarArrowRight from 'components/svg/CalendarArrowRight'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import {
 
   getMonthStart,
@@ -18,6 +18,7 @@ import {
 import { useTranslation } from 'next-i18next'
 import ProjectCalendarSideBarCalendarCell
   from "components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/ProjectCalendarSideBarCalendar/ProjectCalendarSideBarCalendarCell";
+import { useResize } from 'components/hooks/useResize'
 
 
 interface Props {
@@ -27,10 +28,10 @@ interface Props {
 const views = ['decade', 'year', 'month']
 
 export default function ProjectCalendarSideBarCalendar(props: Props) {
-  const {value, onChange} = props
+  const { value, onChange } = props
   const [activeStartDate, setActiveStartDate] = useState(getMonthStart(new Date()))
   const [view, setView] = useState('month')
-  const {t, i18n} = useTranslation('common')
+  const { t, i18n } = useTranslation('common')
 
   useEffect(() => {
     const beginOfMonth = new Date(value.getFullYear(), value.getMonth(), 1)
@@ -44,12 +45,12 @@ export default function ProjectCalendarSideBarCalendar(props: Props) {
   const handleNextClick = () => {
     setActiveStartDate(getBeginNext(view, activeStartDate))
   }
-  const handleViewChange = ({view, activeStartDate, value}) => {
+  const handleViewChange = ({ view, activeStartDate, value }) => {
     setView(view)
     setActiveStartDate(activeStartDate)
   }
   const handleDrillUp = () => {
-    if(views.indexOf(view) === 0){
+    if (views.indexOf(view) === 0) {
       return
     }
     const nextView = views[views.indexOf(view) - 1]
@@ -58,44 +59,46 @@ export default function ProjectCalendarSideBarCalendar(props: Props) {
     setActiveStartDate(nextActiveStartDate)
   }
 
+  const { isLPhoneWidth } = useResize()
+
   const renderLabel = (date) => {
-      switch (view) {
-        case 'century':
-          return getCenturyLabel(i18n.language, formatYear, date)
-        case 'decade':
-          return getDecadeLabel(i18n.language, formatYear, date)
-        case 'year':
-          return formatYear(i18n.language, date)
-        case 'month':
-          const parts = formatMonthYear(i18n.language, date).split(' ')
-          return (<>{parts[0]} <span className={styles.year}>{parts[1]}</span></>)
-        default:
-          throw new Error(`Invalid view: ${view}.`)
-      }
+    switch (view) {
+      case 'century':
+        return getCenturyLabel(i18n.language, formatYear, date)
+      case 'decade':
+        return getDecadeLabel(i18n.language, formatYear, date)
+      case 'year':
+        return formatYear(i18n.language, date)
+      case 'month':
+        const parts = formatMonthYear(i18n.language, date).split(' ')
+        return (<>{isLPhoneWidth ? parts[0].slice(0, 3) : parts[0]} <span className={styles.year}>{parts[1]}</span></>)
+      default:
+        throw new Error(`Invalid view: ${view}.`)
     }
-    return (
-      <div className={styles.root}>
-        <div className={styles.toolbar}>
-          <div className={styles.label} onClick={handleDrillUp}>{renderLabel(activeStartDate)}</div>
-          <div className={styles.arrows}>
-            <div className={styles.arrow} onClick={handlePrevClick}><CalendarArrowLeft/></div>
-            <div className={styles.arrow} onClick={handleNextClick}><CalendarArrowRight/></div>
-          </div>
-        </div>
-        <Calendar
-          className={styles.calendar}
-          onChange={onChange}
-          value={value}
-          locale={i18n.language}
-          activeStartDate={activeStartDate}
-          defaultActiveStartDate={activeStartDate}
-          showNavigation={false}
-          onViewChange={handleViewChange}
-          view={view as any}
-          tileContent={({activeStartDate, date, view}) => view === 'month' ? (
-            <ProjectCalendarSideBarCalendarCell date={date}/>) : null}
-        />
-      </div>
-    )
   }
+  return (
+    <div className={styles.root}>
+      <div className={styles.toolbar}>
+        <div className={styles.label} onClick={handleDrillUp}>{renderLabel(activeStartDate)}</div>
+        <div className={styles.arrows}>
+          <div className={styles.arrow} onClick={handlePrevClick}><CalendarArrowLeft /></div>
+          <div className={styles.arrow} onClick={handleNextClick}><CalendarArrowRight /></div>
+        </div>
+      </div>
+      {!isLPhoneWidth && <Calendar
+        className={styles.calendar}
+        onChange={onChange}
+        value={value}
+        locale={i18n.language}
+        activeStartDate={activeStartDate}
+        defaultActiveStartDate={activeStartDate}
+        showNavigation={false}
+        onViewChange={handleViewChange}
+        view={view as any}
+        tileContent={({ activeStartDate, date, view }) => view === 'month' ? (
+          <ProjectCalendarSideBarCalendarCell date={date} />) : null}
+      />}
+    </div>
+  )
+}
 
--- components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/index.tsx
+++ components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/index.tsx
@@ -3,13 +3,14 @@ import styles from './index.module.scss'
 import CalendarSideBarCalendar from 'components/Calendar/components/CalendarSideBar/CalendarSideBarCalendar'
 import CalendarSideBarEvents from 'components/Calendar/components/CalendarSideBar/CalendarSideBarEvents'
 import Plus from 'components/svg/Plus'
-import { IRootState} from 'types'
+import { IRootState } from 'types'
 
 import { useSelector, useDispatch } from 'react-redux'
-import {format, isSameDay} from 'date-fns'
-import {useEventCalendarContext} from "context/event_calendar";
+import { format, isSameDay } from 'date-fns'
+import { useEventCalendarContext } from "context/event_calendar";
 import ProjectCalendarSideBarCalendar
   from "components/for_pages/Project/ProjectModal/Tabs/TabCalendar/ProjectCalendarSideBar/ProjectCalendarSideBarCalendar";
+import { useResize } from 'components/hooks/useResize'
 
 interface Props {
   currentDate: Date,
@@ -22,23 +23,26 @@ interface Props {
 export default function ProjectCalendarSideBar(props: Props) {
   const calendarContext = useEventCalendarContext()
   const eventsSidebar = calendarContext.sidebarEvents
-  const {currentDate, onChange, onCreate, onClickEvent} = props
+  const { currentDate, onChange, onCreate, onClickEvent } = props
   const getTomorrowDate = () => {
     const tomorrow = new Date()
     tomorrow.setDate(tomorrow.getDate() + 1)
     return tomorrow
   }
+
+  const { isLPhoneWidth } = useResize()
+
   return (
     <div className={`${styles.root}`}>
       <div className={styles.toolbar}>
-        <div className={styles.timezone}>{format(new Date(), 'zzzz')}</div>
-        <div className={styles.create} onClick={onCreate}><Plus/></div>
+        {!isLPhoneWidth && <div className={styles.timezone}>{format(new Date(), 'zzzz')}</div>}
+        {!isLPhoneWidth && <div className={styles.create} onClick={onCreate}><Plus /></div>}
       </div>
-     <ProjectCalendarSideBarCalendar value={currentDate} onChange={onChange}/>
-     <div className={styles.events}>
-      <CalendarSideBarEvents onClickEvent={onClickEvent} type={'today'} events={eventsSidebar.filter(item => isSameDay(new Date(), item.actualStart))}/>
-      <CalendarSideBarEvents onClickEvent={onClickEvent}  type={'tomorrow'} events={eventsSidebar.filter(item => isSameDay(getTomorrowDate(), item.actualStart))}/>
-     </div>
+      <ProjectCalendarSideBarCalendar value={currentDate} onChange={onChange} />
+      {!isLPhoneWidth && <div className={styles.events}>
+        <CalendarSideBarEvents onClickEvent={onClickEvent} type={'today'} events={eventsSidebar.filter(item => isSameDay(new Date(), item.actualStart))} />
+        <CalendarSideBarEvents onClickEvent={onClickEvent} type={'tomorrow'} events={eventsSidebar.filter(item => isSameDay(getTomorrowDate(), item.actualStart))} />
+      </div>}
     </div>
   )
 }
